521c596399a924873d30522015255f7e44c11967
Pawan021 <pawank9870@gmail.com>
2024-05-11 22:32:30 +0530

    Portfolio

---
+++ Script.js
@@ -0,0 +1,6 @@
+function ToggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("Open")
+    icon.classList.toggle("Open")
+}
